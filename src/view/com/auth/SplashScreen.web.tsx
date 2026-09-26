0974fb6ee336b9d0ae39c0d384e59742b785ab3e
lemanhlinh714 <le5149699@gmail.com>
2026-09-26 22:31:59 +0700

    chore: simplify web splash footer
    Remove business, blog, and jobs links from the web splash screen footer.

--- src/view/com/auth/SplashScreen.web.tsx
+++ src/view/com/auth/SplashScreen.web.tsx
@@ -18,7 +18,6 @@ import {AppLanguageDropdown} from '#/components/AppLanguageDropdown'
 import {Button, ButtonText} from '#/components/Button'
 import {TimesLarge_Stroke2_Corner0_Rounded as TimesIcon} from '#/components/icons/Times'
 import * as Layout from '#/components/Layout'
-import {InlineLinkText} from '#/components/Link'
 import {Text} from '#/components/Typography'
 
 export const SplashScreen = ({
@@ -146,7 +145,6 @@ export const SplashScreen = ({
 
 function Footer() {
   const t = useTheme()
-  const {_} = useLingui()
 
   return (
     <View
@@ -164,24 +162,6 @@ function Footer() {
         a.flex_1,
         t.atoms.border_contrast_medium,
       ]}>
-      <InlineLinkText
-        label={_(msg`Learn more about Bluesky`)}
-        to="https://bsky.social">
-        <Trans>Business</Trans>
-      </InlineLinkText>
-      <InlineLinkText
-        label={_(msg`Read the Bluesky blog`)}
-        to="https://bsky.social/about/blog">
-        <Trans>Blog</Trans>
-      </InlineLinkText>
-      <InlineLinkText
-        label={_(msg`See jobs at Bluesky`)}
-        to="https://bsky.social/about/join">
-        <Trans comment="Link to a page with job openings at Bluesky">
-          Jobs
-        </Trans>
-      </InlineLinkText>
-
       <View style={a.flex_1} />
 
       <AppLanguageDropdown />
